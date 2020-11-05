2365b5eda40baaed897dedef4cfd0b9819e1609a
jju72 <jju72@DESKTOP-A886BHJ>
2020-11-05 19:38:45 +0900

    news

--- ZEXEN/src/com/sist/model/NewsModel.java
+++ ZEXEN/src/com/sist/model/NewsModel.java
@@ -69,7 +69,7 @@ public class NewsModel {
 				{
 					cookies[i].setPath("/");
 					String no=cookies[i].getValue();
-					System.out.println(no);
+					
 					NewsVO vo=NewsDAO.newsDetailData(Integer.parseInt(no));
 					oList.add(vo);
 				}
@@ -96,6 +96,8 @@ public class NewsModel {
 		return "redirect:../news/detail.do?news_no="+news_no;
 	}
 	//======================================
+	
+	
 	
 	@RequestMapping("news/list.do")
 	public String list(HttpServletRequest request) {
@@ -149,16 +151,54 @@ public class NewsModel {
 			
 			NewsVO vo=NewsDAO.newsDetailData(news_no);
 			List<ReplyVO> list=NewsDAO.newsReplyListData(news_no);
-			
 			request.setAttribute("vo", vo);
-			
 			request.setAttribute("nList", list);
+			
+			//================찜====================
+			HttpSession session=request.getSession();
+			String id=(String)session.getAttribute("id");
+			if(id!=null) {
+				WishVO jvo=new WishVO();
+				jvo.setId(id);
+				jvo.setNews_no(news_no);
+				int jcount=NewsDAO.newsWishCount(jvo);
+				request.setAttribute("jcount", jcount);
+			}
+			//================찜====================
+			
+			
 			request.setAttribute("main_jsp", "../news/detail.jsp");
-			
-			
-			
-		return "../main/main.jsp";
+			return "../main/main.jsp";
 	}
+	
+	
+	@RequestMapping("news/wish_insert.do")
+	public String news_wish_insert(HttpServletRequest request) {
+		String news_no=request.getParameter("news_no");
+		HttpSession session=request.getSession();
+		String id=(String)session.getAttribute("id");
+		
+		WishVO vo=new WishVO();
+		vo.setId(id);
+		vo.setNews_no(Integer.parseInt(news_no));
+		NewsDAO.newsWishInsert(vo);
+		return "redirect:../news/detail.do?news_no="+news_no;
+	}
+	
+	@RequestMapping("news/wish_delete.do")
+	public String news_wish_delete(HttpServletRequest request) {
+		String news_no=request.getParameter("news_no");
+		HttpSession session=request.getSession();
+		String id=(String) session.getAttribute("id");
+		
+		WishVO vo=new WishVO();
+		vo.setId(id);
+		vo.setNews_no(Integer.parseInt(news_no));
+		NewsDAO.newsWishDelete(vo);
+		return "redirect:../news/detail.do?news_no="+news_no;
+	}
+	
+	
 	
 	@RequestMapping("news/like.do")
 	public String news_like(HttpServletRequest request) {
@@ -212,7 +252,63 @@ public class NewsModel {
 	}
 	
 
-	
+	@RequestMapping("news/search.do")
+    public String news_search(HttpServletRequest request)
+    {
+       String key=request.getParameter("key");
+       String page = request.getParameter("page");
+       if(page==null)
+  			page="1";
+       String sort=request.getParameter("sort");
+       if(sort==null)
+  			sort="1";
+  		
+  		System.out.println(sort);
+  	
+  		switch(sort){
+       case "1": 
+           sort="news_no";
+           break;
+       case "2":
+       	sort="like_cnt DESC";
+           break;
+  		}
+       if (page == null)
+          page = "1";
+       
+       int curpage = Integer.parseInt(page);
+       
+       int rowSize = 3;
+       int start = (curpage * rowSize) - (rowSize - 1);
+       int end = curpage * rowSize;
+
+       Map map = new HashMap();
+       map.put("key", key);
+       map.put("start", start);
+       map.put("end", end);
+       map.put("sort",sort);
+       
+       List<NewsVO> list=NewsDAO.newsSearchData(map);
+       int totalpage=NewsDAO.newsSearchTotalPage(map);
+       
+       int BLOCK = 5;
+       int startPage = ((curpage - 1) / BLOCK * BLOCK) + 1;
+       int endPage = ((curpage - 1) / BLOCK * BLOCK) + BLOCK;
+
+       if(endPage>totalpage)
+          endPage = totalpage;
+
+
+       request.setAttribute("list", list);
+       request.setAttribute("curpage", curpage);
+       request.setAttribute("totalpage", totalpage);
+       request.setAttribute("BLOCK", BLOCK);
+       request.setAttribute("startPage", startPage);
+       request.setAttribute("endPage", endPage);
+       
+       return "../news/list.jsp";
+    }
+   
 	
 	
 }
